cadc074357045a9cff91fce1c436d89e0ccc79bc
Your Name <angelicrf@gmail.com>
2021-12-13 13:37:08 -0800

    initial shopify app

---
+++ newecommerce/pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Angelique 2021 Shopify Using Node And React</Heading>
+  </Page>
+);
+
+export default Index;
